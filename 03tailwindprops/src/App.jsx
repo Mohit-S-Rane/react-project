e179addfee11035fe57f98c14425ba85eac6a781
MSRane <mohitsrane16@gmail.com>
2023-10-25 22:27:21 +0530

    Custom Component with props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -4,11 +4,17 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "hitesh",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
-      <Card />
+      <Card username="hitesh" btnText="Visit Me" objectData={myObj}/>
+      <Card username="Rahul" />
     </>
   )
 }
